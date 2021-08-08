31eec8977d63d270042646fcdc2ae11a468b915a
Rula Al-qasem <jaradatrula6@gmail.com>
2021-08-08 20:16:09 +0300

    done

---
+++ src/components/header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>RESTy</h1>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
+
+
+function Header() {
+  return (
+    <header>
+      <h1>RESTy</h1>
+    </header>
+  )
+}
+
+export default Header
